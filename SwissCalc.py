f39b78a90da159a286dc9ec991059b8b13f9aff5
pjukers <114888132+pjukers@users.noreply.github.com>
2022-10-27 18:50:03 -0400

    FIXED kinetic Energry + more
    You can know calculate agin without running the script again

--- SwissCalc.py
+++ SwissCalc.py
@@ -1,4 +1,7 @@
 import math
+import os
+import sys
+
 print("Welcome.")
 print("Type the number of the operation you select.")
 print("Math:")
@@ -8,11 +11,11 @@ print("3. Multiplication✖")
 print("4. Division➗")
 print("5. Area Calculator(Rectangle)")
 print("6. Area Calculator(circle)")
-print("7. Farenheight to Celsius")
-print("8. Celsius to Farenheight")
+print("7. Fahrenheit to Celsius")
+print("8. Celsius to Fahrenheit")
 print("9. Hypotenuse calculator")
 print("10. Area Calculator (Prism)")
-print("11. Kinetic Energry Calculator")
+print("11. Kinetc Energy Calculator(fixed)")
 oper = input("Enter it here: ")
 if oper == "1":
     add1 = float(input("enter first number: "))
@@ -20,7 +23,8 @@ if oper == "1":
     addsum = add1 + add2
     print(f"the sum is: {addsum}")
     if addsum == 69420.0:
-        print("💀")
+        print("Stop it, get some help.")
+
 
 elif oper == "2":
     sub1 = float(input("enter first number: "))
@@ -52,14 +56,14 @@ elif oper == "6":
     print(f"The area of the circle is {areac}cm^2")
 
 elif oper == "7":
-    tempf = float(input("Enter the tempature in Farhenheight: "))
+    tempf = float(input("Enter the temperature in Fahrenheit: "))
     tempf = round((tempf - 32) * 5 / 9, 1)
-    print(f"The tempature in Celsius is {tempf} degrees")
+    print(f"The temperature in Celsius is {tempf} degrees")
 
 elif oper == "8":
-    tempc = float(input("Enter The tempature in Celsius: "))
+    tempc = float(input("Enter The temperature in Celsius: "))
     tempc = round((9 * tempc) / 5 + 32, 1)
-    print(f"The tempature in Farhenheight is {tempc}")
+    print(f"The temperature in Fahrenheit is {tempc}")
 
 elif oper == "9":
     a = float(input("Enter side A: "))
@@ -75,13 +79,30 @@ elif oper == "10":
     print(f"the area is {area}cm^2")
 
 elif oper == "11":
-    ma = float(input("Enter the mass: "))
-    ve = float(input("Enter the velocity squared : "))
-    KE = 0.5 * ma * ve
-    print(f"The kinetic energy required is {KE}")
+    m11_string = input("Enter the object's mass in kilograms: ")
+    m11 = float(m11_string)
+    v11_string = input("Enter the object's speed in meters per second: ")
+    v11 = float(v11_string)
+
+    e = 0.5 * m11 * v11 * v11
+    print("The object has " + str(e) + " joules of energy.")
+
+
 
 
 
 
 else:
     print("Invalid operation")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+RunAgain = input("calculate again?(y/n): ")
+
+if RunAgain == "y":
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+elif RunAgain == "n":
+    print("See you next time.")
+
+else:
+    "Invalid Input"
